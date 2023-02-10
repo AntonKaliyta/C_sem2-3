9e0978c4b7bbf351b8c268d94d16eecf6c62917f
«anton» <kaluta99@gmail.com>
2023-02-11 06:23:58 +0700

    1 и 3 задача решены, 2 не решена

---
+++ Дз_Семинар_2/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// System.Console.WriteLine("Введите трехначное число");
+// int a = int.Parse(Console.ReadLine());
+// if (a >= 1000 || a <= 99)
+// {
+//     System.Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число");
+// }
+// else
+// {
+//     int result = (a - (a / 100) * 100 - (a % 10))/10;
+//     System.Console.WriteLine(result);
+// }
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.(не решена)
+
+// System.Console.WriteLine("Введите числа");
+// int[] arr = numbers.ToArray();
+// int x = array.Length;
+// if (x < 2) { System.Console.WriteLine("третьей цифры нет"); }
+// else
+// {
+//     System.Console.WriteLine(array[2]);
+// }
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+System.Console.WriteLine("Введите цифру");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7)
+{
+    System.Console.WriteLine("Да");   
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
