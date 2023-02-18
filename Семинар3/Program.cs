fc103b45fd7146228a4df0bc279d19fba25e91ae
«anton» <kaluta99@gmail.com>
2023-02-19 03:12:38 +0700

    Семинар 3 задачи решены

---
+++ Семинар3/Program.cs
@@ -0,0 +1,102 @@
+﻿
+
+// System.Console.WriteLine("Введите значение х: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите значение y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (x > 0 && y > 0)
+// {
+// System.Console.WriteLine("1 четверть");
+// }
+// else if (x<0&&y>00)
+// {
+//     System.Console.WriteLine("2 четверть");
+// }
+// else if (x<0&&y<00)
+// {
+//     System.Console.WriteLine("3 четверть");
+// }
+// else if (x>0&&y<00)
+// {
+//     System.Console.WriteLine("4 четверть");
+// }
+// else
+// {
+// System.Console.WriteLine("Не существует");
+// }
+
+
+// void quater(int x, int y)
+// {
+// if (x > 0 && y > 0)
+// {
+//     System.Console.WriteLine("1 четверть");
+// }
+// else if (x < 0 && y > 00)
+// {
+//     System.Console.WriteLine("2 четверть");
+// }
+// else if (x < 0 && y < 00)
+// {
+//     System.Console.WriteLine("3 четверть");
+// }
+// else if (x > 0 && y < 00)
+// {
+//     System.Console.WriteLine("4 четверть");
+// }
+// else
+// {
+//     System.Console.WriteLine("Не существует");
+// }
+// }
+// System.Console.WriteLine("Введите значение х: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите значение y: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// quater(x1,y1);
+
+// Программа по заданному номеру четверти показывает диапазон возможных координат в этой четверти (x,y)
+
+// void quater(int num)
+// {
+//     if (num == 1)
+//     {
+//         System.Console.WriteLine("x>0,y>0");
+//     }
+//     else if (num == 2)
+//     {
+//         System.Console.WriteLine("x<0,y>0");
+//     }
+//     else if (num == 3)
+//     {
+//         System.Console.WriteLine("x<0,y<0");
+//     }
+//     else if (num == 4)
+//     {
+//         System.Console.WriteLine("x>0,y<0");
+//     }
+// }
+//     System.Console.WriteLine("Введите номер четверти координат[1,2,3,4]");
+//     int quater_number = Convert.ToInt32(Console.ReadLine());
+//     quater(quater_number);
+
+//Программа принимает на вход координаты 2х точек и находит рассояние между ними
+
+// void dist(double x1, double y1, double x2, double y2)
+// {
+//     double distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + (Math.Pow(y1 - y2,2))));
+//     System.Console.WriteLine($"Расстояние между точками равно {distance} ");
+// }
+
+// System.Console.WriteLine("Введите x первой точки ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите y первой точки ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите x второй точки ");
+// int xx = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите y второй точки ");
+// int yy = Convert.ToInt32(Console.ReadLine());
+// dist(x, y, xx, yy);
+
+
+
